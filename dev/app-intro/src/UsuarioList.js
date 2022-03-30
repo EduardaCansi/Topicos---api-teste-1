ca10182deab50188ee565d57a323311ab5cdbdd7
Eduarda <182738@upf.br>
2022-03-29 22:10:06 -0300

    Primeiro commit App Intro

---
+++ dev/app-intro/src/UsuarioList.js
@@ -0,0 +1,41 @@
+const UsuarioList = (props) => {
+
+    return (
+        <div>
+            <h4>Manter Usuários</h4>
+
+            <button onClick={props.onClickAtualizar} type="button"
+                class="btn btn-primary btn-sm">Atualizar Lista</button>
+            <button type="button" class="btn btn-primary btn-sm">Inserir</button>
+
+            <table className='table'>
+                <thead>
+                    <tr>
+                        <th>Index</th><th>Id</th><th>Nome</th><th>Email</th><th>Celular</th><th>Operações</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {props.usuarios.length > 0 ? (props.usuarios.map((o, index) => (
+                        <tr key={index}>
+                            <td>{index}</td>
+                            <td>{o.id}</td>
+                            <td>{o.nome}</td>
+                            <td>{o.email}</td>
+                            <td>{o.celular}</td>
+                            <td>
+                                <button class="btn btn-warning">Editar</button>
+                                <button class="btn btn-danger"
+                                onClick={() => props.excluir(o.id)}>Excluir</button>
+                            </td>
+                        </tr>
+                    ))) : (
+                        <tr>
+                            <td colSpan={5}>Nenhum usuário.</td>
+                        </tr>
+                    )}
+                </tbody>
+            </table>
+        </div>
+    )
+}
+export default UsuarioList
