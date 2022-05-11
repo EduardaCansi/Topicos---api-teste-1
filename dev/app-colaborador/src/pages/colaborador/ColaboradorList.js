797caa5bc0b91ad5142e641a5d4cdb6aac001c08
Eduarda <182738@upf.br>
2022-05-10 22:17:02 -0300

    Paginação, filtro e ordenação

--- dev/app-colaborador/src/pages/colaborador/ColaboradorList.js
+++ dev/app-colaborador/src/pages/colaborador/ColaboradorList.js
@@ -1,4 +1,24 @@
+import { DataTable } from "primereact/datatable";
+import { Column } from "primereact/column";
+
 const ColaboradorList = (props) => {
+  const operacoes = (row) => (
+    <>
+      <button
+        onClick={() => props.editar(row._id)}
+        className="btn btn-primary btn-sm"
+      >
+        Editar
+      </button>
+      <button
+        onClick={() => props.excluir(row._id)}
+        className="btn btn-danger btn-sm"
+      >
+        Excluir
+      </button>
+    </>
+  );
+
   return (
     <div>
       <h4>Manter Colaborador</h4>
@@ -19,48 +39,26 @@ const ColaboradorList = (props) => {
         Inserir
       </button>
 
-      <table className="table">
-        <thead>
-          <tr>
-            <th>Index</th>
-            <th>Id</th>
-            <th>Nome</th>
-            <th>Email</th>
-            <th>Senha</th>
-          </tr>
-        </thead>
-        <tbody>
-          {props.colaboradores.length > 0 ? (
-            props.colaboradores.map((o, index) => (
-              <tr key={index}>
-                <td>{index}</td>
-                <td>{o._id}</td>
-                <td>{o.nome}</td>
-                <td>{o.email}</td>
-                <td>{o.senha}</td>
-                <td>
-                  <button
-                    onClick={() => props.editar(o._id)}
-                    className="btn btn-primary btn-sm"
-                  >
-                    Editar
-                  </button>
-                  <button
-                    onClick={() => props.excluir(o._id)}
-                    className="btn btn-danger btn-sm"
-                  >
-                    Excluir
-                  </button>
-                </td>
-              </tr>
-            ))
-          ) : (
-            <tr>
-              <td colSpan={5}>Nenhum colaborador.</td>
-            </tr>
-          )}
-        </tbody>
-      </table>
+      <div className="card">
+        <DataTable
+          value={props.colaboradores}
+          paginator
+          responsiveLayout="scroll"
+          paginatorTemplate="CurrentPageReport FirstPageLink PrevPageLink PageLinks NextPageLink LastPageLink RowsPerPageDropdown"
+          currentPageReportTemplate="Showing {first} to {last} of {totalRecords}"
+          rows={10}
+          rowsPerPageOptions={[10, 20, 50]}
+          selectionMode="single"
+          selection={props.colaborador}
+          onSelectionChange={(e) => props.setColaboradores(e.value)}
+        >
+          <Column field="_id" header="Id" sortable></Column>
+          <Column field="nome" header="Nome" sortable filter></Column>
+          <Column field="email" header="Email" sortable filter></Column>
+          <Column field="senha" header="Senha" sortable></Column>
+          <Column header="Operações" body={operacoes}></Column>
+        </DataTable>
+      </div>
     </div>
   );
 };
