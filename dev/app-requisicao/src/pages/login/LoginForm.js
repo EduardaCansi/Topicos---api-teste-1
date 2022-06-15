e4bc1fbaea887bd5b0a7445f79fcdafb82e9b2ed
Eduarda <182738@upf.br>
2022-06-15 20:15:09 -0300

    Ajustes finais tela de Login

--- dev/app-requisicao/src/pages/login/LoginForm.js
+++ dev/app-requisicao/src/pages/login/LoginForm.js
@@ -31,11 +31,28 @@ const LoginForm = (props) => {
 
     return (
         <form onSubmit={handleSubmit(onSubmit)}>
+
             <Toast ref={toastRef} />
             <div className="card"
-                style={{ width: '200px', textAlign: "center", marginLeft: '40%', marginTop: '15%' }}>
+                style={{ width: '500px', textAlign: "center", marginLeft: '30%', marginTop: '10%', backgroundColor: "Honeydew" }}>
+                <h2 style={{ textAlign: "center" }}>Login</h2>
+
+                <br></br>
                 <div className="p-fluid grid formgrid">
-                    <div className="field col-4 md:col-4">
+                    <div className="field col-12 md:col-4">
+                        <label htmlFor="email">Email</label>
+                        <InputText type={'text'} id="email" className="p-inputtext-sm block mb-2"
+                            {...register("email", {
+                                required: { value: true, message: "Email é obrigatória" },
+                                minLength: { value: 2, message: "Tamanho mínimo é 2" },
+                            })}
+                            defaultValue={credenciais.email} onKeyUp={handleInputChange} />
+                        {errors.email && <span style={{ color: 'red' }}>{errors.email.message}</span>}
+                    </div>
+                </div>
+
+                <div className="p-fluid grid formgrid">
+                    <div className="field col-12 md:col-4">
                         <label htmlFor="senha">Senha</label>
                         <InputText type={'password'} id="senha" className="p-inputtext-sm block mb-2"
                             {...register("senha", {
@@ -47,7 +64,9 @@ const LoginForm = (props) => {
                     </div>
                 </div>
                 <br />
-                <Button icon="pi pi-sign-in" type="submit" label="Login" className="p-button-text" />
+                <div style={{ textAlign: "center", padding: 8 }}>
+                    <Button icon="pi pi-sign-in" type="submit" label="Login" className="p-button-raised p-button-rounded p-button-text" />
+                </div>
             </div>
         </form>
     );
